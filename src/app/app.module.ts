7e4f91805569ac5b0635f241a05187361ca0e069
Abhi-2209 <you@example.com>
2022-01-11 00:00:30 +0530

    MyFirstCommit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NutritionComponent } from './nutrition/nutrition.component';
+import { WorkoutComponent } from './workout/workout.component';
+import { MerchandiseComponent } from './merchandise/merchandise.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactComponent } from './contact/contact.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NutritionComponent,
+    WorkoutComponent,
+    MerchandiseComponent,
+    AboutUsComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
